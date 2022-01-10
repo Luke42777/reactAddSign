b8b6911b84431b962ddcf33d42de16e7df90a1a7
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-10 12:50:46 +0000

    App done using another component

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ class App extends React.Component {
         text: ""
     }
     handleClick = () => {
-       //this.state.text += "b";
-       this.setState({
-           text: this.state.text + "b"
-       })
+        const nr = Math.floor(Math.random() * 10);
+        this.setState((prevState) => (
+            {
+                text: prevState.text + nr
+            })
+        )
     }
     render() {
         return (
             <>
-                <button onClick={this.handleClick}>Add "b"</button>
-                <h1>{this.state.text}</h1>
+                <button onClick={this.handleClick}>Add number</button>
+                <Display text={this.state.text}>bbbb</Display>
             </>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Display = (props) => {
+    return (
+    < h1 > { props.text }</h1 >)
+    
+}
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("root"));
